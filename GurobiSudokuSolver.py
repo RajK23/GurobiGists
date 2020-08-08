50a4a588897138b09c1938578defedbd35760c50
Kausta <kau23@live.com>
2020-08-08 11:07:43 +1000

    Added Preassigned Slots

--- GurobiSudokuSolver.py
+++ GurobiSudokuSolver.py
@@ -2,7 +2,8 @@ from gurobipy import *
 
 
 '''
-Simple MIP Formulation to solve Sudoku
+Formulation:
+Simple MIP Formulation to Solve Diagonal Sudoku
 
 Sets:
 I : rows
@@ -11,7 +12,8 @@ K : numbers {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 
 Data:
-Presolved_ij 
+pre_ij : Preassigned Squares
+
 
 
 
@@ -19,6 +21,33 @@ Presolved_ij
 
 #Sets
 
+# Pre = [ 
+#         [6,4,0, 0,0,7, 0,2,0], 
+#         [0,0,2, 0,4,0, 0,0,0], 
+#         [3,0,0, 9,6,2, 0,0,0],
+
+#         [5,0,0, 0,0,0, 0,0,0], 
+#         [4,9,0, 0,0,0, 0,7,2], 
+#         [0,0,0, 0,0,0, 0,0,5],
+
+#         [0,0,0, 7,2,3, 0,0,6], 
+#         [0,0,0, 0,9,0, 2,0,0], 
+#         [0,3,0, 1,0,0, 0,4,9]
+#     ]
+
+Pre = [ 
+        [0,4,0, 8,0,0, 0,6,0], 
+        [0,0,0, 0,0,0, 9,3,0], 
+        [0,0,0, 6,0,3, 0,0,2],
+
+        [2,0,0, 4,0,7, 8,0,0], 
+        [3,0,0, 0,0,0, 0,0,0], 
+        [0,0,0, 0,0,0, 0,0,5],
+
+        [0,0,0, 0,0,0, 0,0,9], 
+        [8,9,0, 0,1,0, 0,2,0], 
+        [6,0,5, 0,2,0, 0,0,0]
+    ]
 I = range(9)
 J = range(9)
 K = range(9)
@@ -26,6 +55,11 @@ K = range(9)
 m = Model();
 
 X = {(i,j,k) : m.addVar(vtype = GRB.BINARY) for i in I for j in J for k in K}
+
+for i in range(9):
+    for j in range(9):
+        if (Pre[i][j] != 0): 
+            m.addConstr(X[i, j, Pre[i][j]-1] == 1)
 
 
 for i in I:
@@ -44,10 +78,10 @@ for j in I:
 for ii in range(3):
     for jj in range(3):
         for k in K:
-            m.addConstr(quicksum(X[i,j,k] for i in range(3*ii,3*ii+3) for j in range(3*jj,3*jj+3))==1)
+            m.addConstr(quicksum(X[i,j,k] for i in range(3 * ii,3 * ii+3) for j in range(3 * jj,3 * jj + 3)) == 1)
 
 for k in K:
-    m.addConstr(quicksum(X[x,x,k] for x in range(9)) == 1)
+    m.addConstr(quicksum(X[n,n,k] for n in range(9)) == 1)
 
 m.optimize();
 
